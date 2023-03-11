5793f53e4108903220bb1653b0694d3dabc193d9
tito <tito657175>
2023-03-10 19:52:31 -0800

    Start making the grid 16x16

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
 const container = document.querySelector('.container');
 
-function getInputValue(input){
-    var input = document.getElementById("userInput").value
-    makeDivChildren(input)// add call to function to pull out input
+function getInputValue(){
+    // var input = document.getElementById("userInput").value
+    makeDivChildren()// add call to function to pull out input
 }
 
-function makeDivChildren(e){
-    for (count = 0; count < e; count++){
+function makeDivChildren(){
+    for (count = 0; count < 16; count++){
         const content = document.createElement('div');
         content.classList.add('content');
         content.textContent = `${count+1}`;
         container.appendChild(content);
-        deleteDiv(e);
+        // deleteDiv(e);
     }
 }
 
