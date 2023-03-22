a9b65ae40549b28d59e42912ee632164ef4648f3
tito <tito657175>
2023-03-21 20:17:15 -0700

    Add delete child function, delete button function and custom grid cuntion

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
+//
+let num = 32;
+
 
 const container = document.querySelector(".container");
 console.log(container);
 
 const btn = document.querySelector(".btn");
-console.log(btn);
+btn.addEventListener("click", function() {
+    makeRowDivs(num);
+}, {once : true});
 
-//btn.addEventListener('click',() => {
-    // once:true;
-    //makeRowDivs();
-//});
+var userInput = document.querySelector(".userInput");
+const submitButton = document.querySelector(".submitButton");
 
-function makeRowDivs() {
-    for (count = 0; count < 32; count++){
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = `${count+1}`;
-        container.appendChild(content);
+submitButton.addEventListener("click", function () {
+    let i = userInput.value;
+    let content = document.querySelector(".content");
+    deleteChild();
+    delButton();
+    makeRowDivs(i);
+}
+);
+
+function makeRowDivs(i) {
+for (count = 0; count < i; count++){
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.textContent = `${count+1}`;
+    container.appendChild(content);
     }
+}
+
+function deleteChild() {
+    var contentClass = document.getElementsByClassName('content');
+    console.log(contentClass);
+    while (contentClass.length > 0 ){
+        contentClass[0].parentNode.removeChild(contentClass[0]);
+    }
+}
+
+function delButton(){
+    var btn = document.getElementsByClassName('btn');
+    while (btn.length > 0 )
+    btn[0].remove();
 }
